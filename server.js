b756daeaca4f2b8ab2291b2f3a92d5cd88fb77f3
Amit Bhalerao <incrallinonebackup@gmail.com>
2020-12-08 19:51:57 +0530

    vegan ui

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.static(__dirname + '/dist'));
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
